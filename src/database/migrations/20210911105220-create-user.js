5b5b09625f2608d8611537c520502f6f18b10f31
andreicristea <andrei.cristea@apicore.de>
2021-09-11 12:56:19 +0200

    feat: try to understand root of watcher sleep in nest js

---
+++ src/database/migrations/20210911105220-create-user.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        unique: true,
+        primaryKey: true,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    return queryInterface.dropTable('Users');
+  },
+};
