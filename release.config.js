2aa3128aa959abd931ed8c7d3e7284ebdd18e013
havenousername <andrei.cristea997@gmail.com>
2022-08-04 00:53:59 +0200

    feat: add github release package

---
+++ release.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "release": {
+    "branches": ["master", "main"],
+  },
+  "plugins": [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    '@semantic-release/github'
+  ],
+}
