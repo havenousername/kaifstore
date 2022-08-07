aec9c003f24f47146844643166079b9bdd480b09
havenousername <andrei.cristea997@gmail.com>
2022-08-07 14:25:10 +0200

    feat: add currencies module endpoint

---
+++ src/backend/currencies/currencies.service.ts
@@ -0,0 +1,37 @@
+type RequestInitCurrencies = {
+  method: string;
+  redirect: RequestRedirect;
+  headers: HeadersInit;
+};
+
+type ResponseCurrenciesData = {
+  success: boolean;
+  symbols: Record<string, string>;
+};
+
+import fetch, { Headers } from 'node-fetch';
+
+export class CurrenciesService {
+  constructor(private apiUrl: string = 'https://api.apilayer.com') {}
+
+  async getCurrencies(): Promise<ResponseCurrenciesData> {
+    const headers = new Headers();
+    headers.append('apikey', 'iuI65C3LZIxIPuiNyyChFpeB95Uh0HmO');
+    const reqOptions: RequestInitCurrencies = {
+      method: 'GET',
+      redirect: 'follow',
+      headers,
+    };
+
+    const data = await fetch(
+      this.apiUrl + '/exchangerates_data/symbols',
+      reqOptions,
+    );
+
+    if (data.status !== 200) {
+      return { success: false, symbols: {} };
+    }
+
+    return (await data.json()) as ResponseCurrenciesData;
+  }
+}
